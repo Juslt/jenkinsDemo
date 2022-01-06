9150ec447d5b030862df01e2880ca38bccd45d3d
Wang Xin <wangxin@dofun.com>
2022-01-06 11:22:09 +0800

    添加配置字段APP_BUILD_TYPE

--- app/src/main/java/com/example/daytestdemo/MainActivity.kt
+++ app/src/main/java/com/example/daytestdemo/MainActivity.kt
@@ -10,8 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.tv_hello_world).setOnClickListener {
-            startActivity(Intent(this,MainActivity2::class.java))
+        findViewById<TextView>(R.id.tv_hello_world).let {
+            it.setOnClickListener {
+                startActivity(Intent(this,MainActivity2::class.java))
+            }
+            it.text = BuildConfig.APP_BUILD_TYPE
         }
     }
 }
